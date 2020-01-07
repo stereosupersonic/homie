25755eedcfb2f0970aeb206ab5ff5e749c92f910
michael <m.deimel@miceportal.com>
2020-01-07 15:01:26 +0100

    add fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,5 @@ require("channels")
 require("jquery");
 require("bootstrap");
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+require("../stylesheets/application")
+import "@fortawesome/fontawesome-free/css/all.css";
